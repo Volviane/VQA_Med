23f226dad5739792639ad49d0544b09830cd8108
Volviane <sapphiremfogo@gmail.com>
2021-01-21 20:52:35 +0100

    hyperparameter tuning

--- config_1.py
+++ config_1.py
@@ -49,6 +49,6 @@ def parse_opt():
     parser.add_argument('--NUM_QUESTION_GLIMPSE', type=int, default=2)
     parser.add_argument('--IMG_FEAT_SIZE', type=int, default=1) #196 #1
     parser.add_argument('--IMG_INPUT_SIZE', type=int, default=224) # 228
-    parser.add_argument('--NUM_EPOCHS', type=int, default=50) #300 
+    parser.add_argument('--NUM_EPOCHS', type=int, default=200) #300 
     args = parser.parse_args()
     return args
