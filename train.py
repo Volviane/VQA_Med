5e925d0011aedb39b93a0df77b8553c071702718
Volviane <sapphiremfogo@gmail.com>
2021-02-18 17:02:41 +0100

    from exp1_6 to public repo

--- train.py
+++ train.py
@@ -266,7 +266,7 @@ def accuracy(output, target, topk=(1,)):
 
     res = []
     for k in topk:
-        correct_k = correct[:k]..reshape(-1).float().sum(0)
+        correct_k = correct[:k].reshape(-1).float().sum(0)
         res.append((correct_k / batch_size))
     # print(res)
     return res
